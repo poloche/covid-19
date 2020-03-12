635d85f039efdfede6883fc2168eeb6aff38a76a
JALASOFT\Paolo Lizarazu <paolo.lizarazu@jalasoft.com>
2020-03-12 18:26:55 -0400

    Initial commit

---
+++ src/main/kotlin/com/example/demo/app/Covid19.kt
@@ -0,0 +1,6 @@
+package com.example.demo.app
+
+import com.example.demo.view.MainView
+import tornadofx.App
+
+class Covid19: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -0,0 +1,105 @@
+package com.example.demo.view
+
+import javafx.collections.FXCollections
+import javafx.scene.chart.CategoryAxis
+import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.XYChart
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import tornadofx.*
+import java.io.File
+import java.io.IOException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+class MainView : View("Covid-19 Follow up") {
+    private val path = "series.txt"
+    private var total = FXCollections.observableArrayList<XYChart.Data<String, Number>>()
+    private var curados = FXCollections.observableArrayList<XYChart.Data<String, Number>>()
+    private var fallecidos = FXCollections.observableArrayList<XYChart.Data<String, Number>>()
+    private var activos = FXCollections.observableArrayList<XYChart.Data<String, Number>>()
+
+    override val root = vbox {
+        label(title) { }
+        linechart("Avance Corona Virus", CategoryAxis(), NumberAxis()) {
+            series("Total Casos", total)
+            series("Curados", curados)
+            series("Fallecidos", fallecidos)
+            series("Activos", activos)
+        }
+    }
+
+    init {
+        loadFromWeb()
+    }
+
+    private fun loadFromWeb() {
+        val input = File("input.html")
+        if (!input.exists()) {
+            input.createNewFile()
+        }
+        val doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get()
+
+        if (!File(path).exists()) {
+            File(path).createNewFile()
+            appendHeader()
+        }
+        //2. Parses and scrapes the HTML response
+        val date = doc.select("div.content-inner").first().child(4)
+        appendText(getFormattedDate(date))
+        doc.select("div#maincounter-wrap").forEachIndexed { index, element ->
+            println(element.text())
+            appendText(getNumericValue(element) + ",")
+        }
+        appendText(System.getProperty("line.separator"))
+
+        loadFromFile()
+    }
+
+    private fun loadFromFile() {
+        var i = 0
+        File(path).forEachLine {
+            if (i != 0) {
+
+                val line = it.split(",")
+                val fecha = line[0]
+                val totalCasos = line[1].trim().toInt()
+                val totalCurados = line[2].trim().toInt()
+                val totalFallecidos = line[3].trim().toInt()
+                total.add(XYChart.Data(fecha, totalCasos))
+                curados.add(XYChart.Data(fecha, totalCurados))
+                fallecidos.add(XYChart.Data(fecha, totalFallecidos))
+                val casosActivos = totalCasos-totalCurados-totalFallecidos
+                activos.add(XYChart.Data(line[0], casosActivos))
+            }
+            i++
+        }
+    }
+
+    private fun getFormattedDate(date: Element): String {
+        var dateString = date.text().split(":")[1]
+        dateString = dateString.substring(0, dateString.length - 4).trim()
+        val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH)
+        return LocalDate.parse(dateString, formatter).toString() + ","
+    }
+
+    private fun appendHeader() {
+        val text = "Date, Total, Curados, Fallecidos, Activos " + System.getProperty("line.separator")
+        appendText(text)
+    }
+
+    private fun appendText(text: String) {
+        try {
+            File(path).appendText(text)
+        } catch (e: IOException) {
+            println("can't add text: $text")
+        }
+    }
+
+    private fun getNumericValue(element: Element): String {
+        val textValue = element.text().split(":")[1]
+        return textValue.replace(",", "").trim().toInt().toString()
+    }
+}
